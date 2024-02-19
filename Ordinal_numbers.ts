3bca634505d9e142655a4afd7942fb1feea26c33
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:39:18 -0800

    Add files via upload

---
+++ Ordinal_numbers.ts
@@ -0,0 +1,23 @@
+//Written by :TAYYABA RAO 
+// Date: 18/feb/2024
+// Roll no : 00388416
+
+// numbers from 1 to 9
+const numbers : number[] = [1 , 2 , 3 ,4 ,5 ,6 ,7 ,8 ,9];
+
+//using a loop.
+for (let i=0; i < numbers.length; i++) {
+
+    const number = numbers[i];
+    let ordinalEnding: string;
+
+if (number ===1){
+    console.log(`${number}st`);
+}else if (number ===2){
+    console.log(`${number}nd`);
+}else if (number ===3) {
+    console.log(`${number}rd`);
+}else{
+    console.log(`${number}th`);
+}
+}
